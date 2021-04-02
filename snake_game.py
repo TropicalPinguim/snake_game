25b7aaf5063b5739c2bdb945d8bd6d46cba2f828
TropicalPinguim <alan_3dworks@outlook.com>
2021-04-01 22:39:41 -0300

    Alan_b

--- snake_game.py
+++ snake_game.py
@@ -1,77 +1,84 @@
-import pygame
+import pygame, CSSColors
 from pygame.locals import *
 from sys import exit
 from random import randint
 
-pygame.init()
+
 
 #--------------------------------------------- declared variables -----------------------------------------------------#
-larger = 640
-altura = 480
 
-x_snake = int(larger / 2)
-y_snake = int(altura / 2)
+width = 600
+height = width
 
-x_apple = int(randint(40, 600))
-y_apple = int(randint(50, 430))
+x_snake = int(width / 2)//20*20
+y_snake = int(height / 2)//20*20
 
-velocidade = 10
+x_apple = (randint(20, 580))//20*20
+y_apple = (randint(20, 580))//20*20
+
+velocidade = 20
 x_control = velocidade
 y_control = 0
 
 pontos = 0
 
+list_snake = []
+complimento_inicial = 4
+died = False
+
+#---------------------------------------------- configuração padrão -----------------------------------------------------#
+
+
+pygame.init()                                           # da inicio ao pygame.
+tela = pygame.display.set_mode((height, width))         # constroi um display é passado a largura e autura como parametro.
+pygame.display.set_caption('Snake_Game')                # define o nome da aplicação.
+clock = pygame.time.Clock()                             # inicia o um relogio para dar inicio aos FPS.
 fonte = pygame.font.SysFont('arial', 30, True, False)
 fonte2 = pygame.font.SysFont('arial', 20, True, True)
 
-tela = pygame.display.set_mode((larger, altura))
+#---------------------------------------------- definindo funções -------------------------------------------------------#
 
-pygame.display.set_caption('Snake_Game')  # essa string represent o nome da aplicação
-
-clock = pygame.time.Clock()
-
-list_snake = []
-complimento_inicial = 5
-
-died = False
+def on_grid_apple_random():
+    global x_apple,y_apple
+    x_apple = (randint(20, 590))//20*20
+    y_apple = (randint(20, 590))//20*20
 
 def snake_plus_plus(list_snake):
     for XeY in list_snake:
-        pygame.draw.rect(tela, (0, 255, 0), (XeY[0], XeY[1], 20, 20))
+        pygame.draw.rect(tela, CSSColors.lime_green, (XeY[0], XeY[1], 20, 20))
 
-def reiniciar_jogo():
+def restart_game():
     global pontos,complimento_inicial,x_snake,y_snake,lista_cabeca,list_snake,x_apple,y_apple,died
     pontos = 0
-    complimento_inicial = 5
-    x_snake = int(larger/2)
-    y_snake = int(altura / 2)
+    complimento_inicial = 2
+    x_snake = int(width/2)//20*20
+    y_snake = int(height/2)//20*20
     lista_cabeca = []
     list_snake = []
-    x_apple = int(randint(40, 600))
-    y_apple = int(randint(50, 430))
+    on_grid_apple_random()
     died = False
 
 def game_over():
     global fonte2,msn2,text_game_over,rect_text,event,died
 
     msn2 = 'GAME OVER aperte R para reiniciar'
-    text_game_over = fonte2.render(msn2,True,(0,0,0))
+    text_game_over = fonte2.render(msn2,True,CSSColors.blue)
     rect_text =  text_game_over.get_rect()
     died = True
     while died:
-        tela.fill((255,255,255))
+        tela.fill(CSSColors.black)
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 exit()
             if event.type == KEYDOWN:
                 if event.key == K_r:
-                    reiniciar_jogo()
-        rect_text.center = (larger // 2, altura // 2)
+                    restart_game()
+        rect_text.center = (width // 2, height // 2)
         tela.blit(text_game_over,rect_text)
         pygame.display.update()
 
-def movimento():
+def control_movement():
     global x_control, y_control
     if event.key == K_a:
         if x_control == velocidade:
@@ -99,26 +106,25 @@ def movimento():
             y_control = -velocidade
 
 def border_colision():
-    global x_snake, y_snake, altura, larger
+    global x_snake, y_snake, height, width
 
-    if x_snake > larger:
+    if x_snake > width:
         x_snake = 0
     if x_snake < 0:
-        x_snake = larger
+        x_snake = width
     if y_snake < 0:
-        y_snake = altura
-    if y_snake > altura:
+        y_snake = height
+    if y_snake > height:
         y_snake = 0
 
-def peguei():
+def grip():
     global x_apple,y_apple,complimento_inicial,pontos
-
-    x_apple = int(randint(40, 600))
-    y_apple = int(randint(50, 430))
+    
+    on_grid_apple_random()
     complimento_inicial += 1
     pontos += 1
 
-def tamanho_snake():
+def size_snake():
     global list_snake,lista_cabeca
 
     lista_cabeca.append(x_snake)
@@ -126,31 +132,38 @@ def tamanho_snake():
     list_snake.append(lista_cabeca)
 
 while True:
-    clock.tick(30)
-    tela.fill((255, 255, 255))
-    snake = pygame.draw.rect(tela, (0, 255, 0), (x_snake, y_snake, 20, 20))
-    apple = pygame.draw.rect(tela, (255, 0, 0), (x_apple, y_apple, 20, 20))
+    clock.tick(20)
+    tela.fill(CSSColors.black)
+    snake = pygame.draw.rect(tela, CSSColors.lime_green, (x_snake, y_snake, 20, 20))
+    apple = pygame.draw.rect(tela, CSSColors.orange_red, (x_apple, y_apple, 20, 20))
+    
     HUD = f'PONTOS: {pontos}'
-    texto_formatado = fonte.render(HUD, False, (0, 0, 0))
+    texto_formatado = fonte.render(HUD, False, CSSColors.antique_white)
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
         if event.type == KEYDOWN:
-            movimento()
+            control_movement()
         x_snake = x_snake + x_control
         y_snake = y_snake + y_control
 
         if snake.colliderect(apple):
-            peguei()
+            grip()
+
         lista_cabeca = []
-        tamanho_snake()
+
+        size_snake()
+
         if list_snake.count(lista_cabeca) > 1:
             game_over()
+
         border_colision()
+
         if len(list_snake) > complimento_inicial:
             del list_snake[0]
 
-        tela.blit(texto_formatado, (450, 40))
-        snake_plus_plus(list_snake)
-        pygame.display.update()
+    tela.blit(texto_formatado, (10, 10))
+    snake_plus_plus(list_snake)
+    pygame.display.update()
